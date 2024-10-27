02d4225fcd81f08ebcd1496968196d2e5913b0f9
MikolajSulkowski <mikolajsulkowski675@gmail.com>
2024-10-27 14:16:33 +0100

    feat: design structure

--- MorseCodeApp/main.py
+++ MorseCodeApp/main.py
@@ -1,10 +1,15 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 
-class MyWidget(Widget):
+class MorseCodeWidget(GridLayout):
+    pass
+
+class MyBoxLayout(BoxLayout):
     pass
 
 class MorseCodeApp(App):
     pass
 
-MorseCodeApp().run()
+if __name__ == '__main__':
+    MorseCodeApp().run()
