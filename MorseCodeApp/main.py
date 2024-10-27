82b4df75272c821db93afd23cf828b2799eabe0b
MikolajSulkowski <mikolajsulkowski675@gmail.com>
2024-10-27 14:24:56 +0100

    chore: some changes in structure

--- MorseCodeApp/main.py
+++ MorseCodeApp/main.py
@@ -1,15 +1,12 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 
 class MorseCodeWidget(GridLayout):
     pass
 
-class MyBoxLayout(BoxLayout):
-    pass
-
 class MorseCodeApp(App):
-    pass
+    def build(self):
+        return MorseCodeWidget()
 
 if __name__ == '__main__':
     MorseCodeApp().run()
